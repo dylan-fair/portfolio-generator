cac66e58da0e1114ee67139702acfa6e0e05d94d
dylan fair <dylanfair11@gmail.com>
2021-10-18 19:12:28 -0500

    added prompts

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [names, github] = profileDataArgs;
-fs.writeFile("index.html", generatePage(names, github), err => {
-  if(err) throw err;
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
-  console.log("Portfolio complete! check out index.html to see ouput!");
-})
+//const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+//  if (err) throw err;
+
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
